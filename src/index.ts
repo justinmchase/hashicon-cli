891f4803f5059815b347984131e6f66a644f45ac
Justin Chase <jchase@structural.com>
2019-08-13 16:00:48 -0500

    basic implementation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,34 @@
-import { createCanvas } from 'canvas'
+import fs from 'fs'
+import path from 'path'
 import hashicon from 'hashicon'
-const icon = hashicon('0xdc53525847b67a9e32d80066202d5744c86ae500', { createCanvas })
-const url = icon.toDataURL()
-console.log(url)
+import { createCanvas } from 'canvas'
+
+import yargs from 'yargs'
+
+const argv = yargs
+  .nargs('hash', 1)
+  .option('data-url', {
+    alias: 'd',
+    describe: 'True to print out a data-url to stdout',
+    boolean: true
+  })
+  .option('out', {
+    alias: 'o',
+    describe: 'Path to file to write the image (png) to',
+    string: true
+  })
+  .help('help')
+  .argv
+
+const { _: [hash], dataUrl, out } = argv
+    
+const icon = hashicon(hash, { createCanvas })
+if (out) {
+
+  const fullPath = path.resolve(out)
+  fs.writeFileSync(fullPath, icon.toBuffer())
+}
+
+if (dataUrl) {
+  console.log(icon.toDataURL())
+}
